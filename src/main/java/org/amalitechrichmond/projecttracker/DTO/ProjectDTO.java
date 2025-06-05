f2146f0aaa56f3d0140bfe459bae643aec3e7a6d
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-05 08:47:39 -0700

    feat(mapper): add project mapper logic to convert POJO from client request to an entity that will be used for database insertion and vice-versa

--- src/main/java/org/amalitechrichmond/projecttracker/DTO/ProjectDTO.java
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/ProjectDTO.java
@@ -1,9 +1,13 @@
 package org.amalitechrichmond.projecttracker.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.amalitechrichmond.projecttracker.enums.ProjectStatus;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 public class ProjectDTO {
     private Long id;
     private String name;
