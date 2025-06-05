422d04cb47c9d5270296ab55cd1740dca0cf870d
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-05 05:42:04 -0700

    feat(DTO): add data transfer object for Developer, Project and Task classes

---
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/DeveloperDTO.java
@@ -0,0 +1,11 @@
+package org.amalitechrichmond.projecttracker.DTO;
+
+import java.util.List;
+
+public class DeveloperDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private List<String> skills;
+}
+
