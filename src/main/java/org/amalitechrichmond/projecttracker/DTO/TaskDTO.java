7359fb1a46626c710c578a11686a691a1613e2ee
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-05 08:51:48 -0700

    feat(DTO): add task data transfer object logic to extract the neccessery data need for request and response

--- src/main/java/org/amalitechrichmond/projecttracker/DTO/TaskDTO.java
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/TaskDTO.java
@@ -1,10 +1,14 @@
 package org.amalitechrichmond.projecttracker.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.amalitechrichmond.projecttracker.enums.TaskStatus;
 
 import java.time.LocalDate;
 import java.util.Set;
 
+@Setter
+@Getter
 public class TaskDTO {
     private Long id;
     private String title;
