58a1546d1d6129d616ad71e6fb52a7ecdd0d7097
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-05 05:33:36 -0700

    feat(model): add Entity structure for Project, Task and Developer models and their corresponding relationships

---
+++ src/main/java/org/amalitechrichmond/projecttracker/model/Developer.java
@@ -0,0 +1,39 @@
+package org.amalitechrichmond.projecttracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @ElementCollection
+    private List<String> skills = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "developers")
+    private Set<Task> tasks = new HashSet<>();
+}
+
