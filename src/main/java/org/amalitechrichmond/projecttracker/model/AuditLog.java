29799ecd01050dd59abcbe1ce21b3a2b02bfef91
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-06 03:20:32 -0700

    feat(model): add model structure for  auditlog

---
+++ src/main/java/org/amalitechrichmond/projecttracker/model/AuditLog.java
@@ -0,0 +1,29 @@
+package org.amalitechrichmond.projecttracker.model;
+
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "audit_logs")
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuditLog {
+    @Id
+    private String id;
+
+    private String actionType;   // CREATE, UPDATE, DELETE
+    private String entityType;   // "Project", "Task", etc.
+    private String entityId;
+    private LocalDateTime timestamp;
+    private String actorName;
+
+    @Lob
+    private String payload; // JSON of the changed entity
+}
+
