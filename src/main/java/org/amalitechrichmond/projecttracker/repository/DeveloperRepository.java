90893d6f8eae1183f06e18337719e66ad77457b9
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-06 05:24:18 -0700

    feat(repository): refactor to use JPQL to findTop5ByOrderByTasksSizeDesc

--- src/main/java/org/amalitechrichmond/projecttracker/repository/DeveloperRepository.java
+++ src/main/java/org/amalitechrichmond/projecttracker/repository/DeveloperRepository.java
@@ -1,7 +1,15 @@
 package org.amalitechrichmond.projecttracker.repository;
 
 import org.amalitechrichmond.projecttracker.model.Developer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+    @Query("SELECT d FROM Developer d LEFT JOIN d.tasks t GROUP BY d ORDER BY COUNT(t) DESC")
+    List<Developer> findTop5ByOrderByTasksSizeDesc();
+
 }
