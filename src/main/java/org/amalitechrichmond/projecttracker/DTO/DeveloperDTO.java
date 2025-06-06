452d81d938c3fe7b15ab016bc18a6262edb8b4af
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-06 03:10:12 -0700

    feat(DTO): upgrade to have validations

--- src/main/java/org/amalitechrichmond/projecttracker/DTO/DeveloperDTO.java
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/DeveloperDTO.java
@@ -3,13 +3,31 @@ package org.amalitechrichmond.projecttracker.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
 import java.util.List;
+import java.util.Set;
+
+@Data
 @Setter
 @Getter
 public class DeveloperDTO {
+
     private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must not exceed 100 characters")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
-    private List<String> skills;
+
+    @NotEmpty(message = "At least one skill is required")
+    private List<@Size(min = 2, message = "Each skill must have at least 2 characters") String> skills;
+
+    private Set<Long> taskIds;
 }
+
 
--- src/main/java/org/amalitechrichmond/projecttracker/DTO/ProjectDTO.java
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/ProjectDTO.java
@@ -1,18 +1,31 @@
 package org.amalitechrichmond.projecttracker.DTO;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.amalitechrichmond.projecttracker.enums.ProjectStatus;
-
 import java.time.LocalDate;
+import jakarta.validation.constraints.*;
 
+@Data
 @Setter
 @Getter
 public class ProjectDTO {
+
     private Long id;
+
+    @NotBlank(message = "Project name is required")
+    @Size(max = 100, message = "Project name must not exceed 100 characters")
     private String name;
+
+    @Size(max = 255, message = "Description must not exceed 255 characters")
     private String description;
+
+    @FutureOrPresent(message = "Deadline must be today or a future date")
     private LocalDate deadline;
+
+//    @NotBlank(message = "Status is required")
     private ProjectStatus status;
 }
+
 
