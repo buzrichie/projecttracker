6165021cf81ff95ec9cc6c2939a3101a4f36bdfd
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-06 05:24:49 -0700

    feat(repository): refactor to use JPQL to findProjectsWithoutTasks

--- src/main/java/org/amalitechrichmond/projecttracker/repository/ProjectRepository.java
+++ src/main/java/org/amalitechrichmond/projecttracker/repository/ProjectRepository.java
@@ -2,6 +2,12 @@ package org.amalitechrichmond.projecttracker.repository;
 
 import org.amalitechrichmond.projecttracker.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("SELECT p FROM Project p WHERE p.tasks IS EMPTY")
+    List<Project> findProjectsWithoutTasks();
+
 }
