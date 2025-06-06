ce6532db8a74136af06a92cbc552375642144bea
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-06 03:10:12 -0700

    feat(DTO): upgrade to have validations

--- src/main/java/org/amalitechrichmond/projecttracker/DTO/TaskDTO.java
+++ src/main/java/org/amalitechrichmond/projecttracker/DTO/TaskDTO.java
@@ -4,18 +4,36 @@ import lombok.Getter;
 import lombok.Setter;
 import org.amalitechrichmond.projecttracker.enums.TaskStatus;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.Set;
 
+@Data
 @Setter
 @Getter
+
 public class TaskDTO {
+
     private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title must not exceed 100 characters")
     private String title;
+
+    @Size(max = 255, message = "Description must not exceed 255 characters")
     private String description;
+
+    @NotBlank(message = "Status is required")
     private TaskStatus status;
+
+    @FutureOrPresent(message = "Due date must be today or in the future")
     private LocalDate dueDate;
+
+    @NotNull(message = "Project ID is required")
     private Long projectId;
+
     private Set<Long> developerIds;
 }
 
