6035f99fae7f66de116048440ba90fb055ab2a7a
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-24 02:36:05 -0700

    refactor(service) change login  response type to AuthResponse DTO

--- src/main/java/org/amalitechrichmond/projecttracker/service/AuthService.java
+++ src/main/java/org/amalitechrichmond/projecttracker/service/AuthService.java
@@ -2,11 +2,12 @@ package org.amalitechrichmond.projecttracker.service;
 
 import org.amalitechrichmond.projecttracker.DTO.AuthLoginRequestDTO;
 import org.amalitechrichmond.projecttracker.DTO.AuthRegisterRequestDTO;
+import org.amalitechrichmond.projecttracker.DTO.AuthResponseDTO;
 import org.amalitechrichmond.projecttracker.DTO.UserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface AuthService {
-    String login(AuthLoginRequestDTO user);
+    AuthResponseDTO login(AuthLoginRequestDTO user);
     UserDTO register(AuthRegisterRequestDTO user);
 }
