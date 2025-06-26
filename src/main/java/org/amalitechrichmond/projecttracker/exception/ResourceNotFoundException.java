077d5a5f529088826e25233c6e9b397e3c6a18c7
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-26 12:27:40 -0700

    feat(exception) add exception response handler for filter chain auth failure

--- src/main/java/org/amalitechrichmond/projecttracker/exception/ResourceNotFoundException.java
+++ src/main/java/org/amalitechrichmond/projecttracker/exception/ResourceNotFoundException.java
@@ -1,11 +1,30 @@
 package org.amalitechrichmond.projecttracker.exception;
 
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    @Component
+    public static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+        @Override
+        public void commence(HttpServletRequest request,
+                             HttpServletResponse response,
+                             AuthenticationException authException) throws IOException, java.io.IOException {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Unauthorized or invalid token\"}");
+        }
     }
 }
