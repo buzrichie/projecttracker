2e7559f29ce7c9e2b49388698b0d8a1e57f356d3
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-28 07:15:09 -0700

    feat(provider) update JwtTokenProvider to use environment string for secret

--- src/main/java/org/amalitechrichmond/projecttracker/provider/JwtTokenProvider.java
+++ src/main/java/org/amalitechrichmond/projecttracker/provider/JwtTokenProvider.java
@@ -2,8 +2,10 @@ package org.amalitechrichmond.projecttracker.provider;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
@@ -11,22 +13,31 @@ import java.util.Map;
 @Component
 public class JwtTokenProvider {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${secret}")
+    private String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String id) {
         return Jwts.builder()
                 .setSubject(id)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // 24 mins
                 .signWith(key)
                 .compact();
     }
 
-    public String generateToken(String id , Map<String, Object> claims) {
+    public String generateToken(String id, Map<String, Object> claims) {
         return Jwts.builder()
+                .setClaims(claims)
                 .setSubject(id)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(key)
                 .compact();
     }
@@ -48,8 +59,8 @@ public class JwtTokenProvider {
                     .parseClaimsJws(token);
             return true;
         } catch (JwtException e) {
-            // invalid token
+            // log the reason if needed
+            return false;
         }
-        return false;
     }
 }
